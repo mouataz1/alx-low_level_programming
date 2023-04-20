46414e0493b7eca02d67bf4f50224f2d6bbc3751
mouataz1 <moataz.hakkou@gmail.com>
2023-04-20 01:30:20 -0700

    f01

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,20 +1,14 @@
-#include <stdio.h>
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name as is
- * @name: name of the person
- * @f: is pointer
-**/
-
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
-	{
 		return;
-	}
-	else
-	{
-		f(name);
-	}
+
+	f(name);
 }
