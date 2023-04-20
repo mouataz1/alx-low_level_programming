7c8cd6a79b3ce05e9a5987293296714a38683655
mouataz1 <moataz.hakkou@gmail.com>
2023-04-20 01:36:35 -0700

    f01

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,12 +1,39 @@
-#ifndef _FUNCTION_POINTERS_H_
-#define _FUNCTION_POINTERS_H_
+#ifndef FUNC_H
+#define FUNC_H
+
 /**
- * File: main
- *
- * Desc: header file
-**/
+ * @brief Header file for function pointers
+ */
+
+#include <stdlib.h>
+
+/**
+ * @brief Prints a name using a given function pointer.
+ * 
+ * @param name A pointer to a character array representing the name.
+ * @param f A function pointer that takes a char pointer and returns void.
+ */
 void print_name(char *name, void (*f)(char *));
+
+/**
+ * @brief Iterates over an integer array and applies a given action function to each element.
+ * 
+ * @param array A pointer to the integer array.
+ * @param size The size of the integer array.
+ * @param action A function pointer that takes an integer and returns void.
+ */
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+/**
+ * @brief Searches for an integer in an array using a given comparison function.
+ * 
+ * @param array A pointer to the integer array.
+ * @param size The size of the integer array.
+ * @param cmp A function pointer that takes an integer and returns an integer.
+ * @return The index of the first element in the array for which the cmp function returns a non-zero value. 
+ * If no such element is found, the function returns -1.
+ */
 int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
+
